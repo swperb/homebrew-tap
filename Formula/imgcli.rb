02bbfb8e6aae710e3ff76cffc3ea184c31ab6d40
Stephen Proctor <stephenproctor291@gmail.com>
2026-06-15 22:17:39 -0500

    imgcli 0.5.0 (man page + README benchmarks)

--- Formula/imgcli.rb
+++ Formula/imgcli.rb
@@ -1,14 +1,15 @@
 class Imgcli < Formula
   desc "Lightweight ffmpeg-style image conversion & processing CLI (no deps)"
   homepage "https://github.com/swperb/imgcli"
-  url "https://github.com/swperb/imgcli/archive/refs/tags/v0.4.0.tar.gz"
-  sha256 "3eedde19ddfa25dc3ed453a94f491b561f92d3e1af6fb2c66929db48e530f644"
+  url "https://github.com/swperb/imgcli/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "4d1b2b4458787502ae651a8f9d798e8b949df22f37bc711658e7939a5c1e7789"
   license "MIT"
   head "https://github.com/swperb/imgcli.git", branch: "main"
 
   def install
     system "make"
     bin.install "imgcli"
+    man1.install "man/imgcli.1"
   end
 
   test do
